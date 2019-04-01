df9e758d40fcfaa7139b6573ef2e13ad0b0cabed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-01 10:13:53 +0000

    Deploy seanlau555/demo-app to github.com/seanlau555/demo-app.git:gh-pages

---
+++ precache-manifest.751ef28a5e7b7e754f9ceddb853423e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "853e7e9c816202c88aef",
+    "url": "/demo-app/static/js/main.853e7e9c.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "853e7e9c816202c88aef",
+    "url": "/demo-app/static/css/main.c8f0da47.chunk.css"
+  },
+  {
+    "revision": "2805de93a72c651eb3eb3338d5d5c697",
+    "url": "/demo-app/index.html"
+  }
+];
